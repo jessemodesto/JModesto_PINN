3642046f82ca6418de1879f9a5bcd64a8b52a7f3
Jesse Modesto <jesse.modesto.resume@protonmail.com>
2024-10-24 09:32:16 -0700

    fix expected test data output

--- cantilever_beam_transient.py
+++ cantilever_beam_transient.py
@@ -4,10 +4,11 @@ import numpy as np
 import types
 
 if __name__ == "__main__":
-    solution = np.array([0.0, 0.0843201354146, 0.000311157549731, 0.00122306111734, 0.00270409253426, 0.00472263386473,
+    solution = np.array([0.0, 0.000311157549731, 0.00122306111734, 0.00270409253426, 0.00472263386473,
                          0.00724706612527, 0.0102457720786, 0.0136871328577, 0.0175395291299, 0.0217713452876,
                          0.0263509619981, 0.0312467608601, 0.0364271253347, 0.0418604314327, 0.0475150682032,
-                         0.0533594116569, 0.0593618489802, 0.0654907599092, 0.0717145204544, 0.0780015215278],
+                         0.0533594116569, 0.0593618489802, 0.0654907599092, 0.0717145204544, 0.0780015215278,
+                         0.0843201354146],
                         dtype=np.float64)
     pinn = PINN(  # constants declared here so that they are not constructed each time in functions
         constants={'rho': tf.constant(value=2770.0, shape=(1, 1), dtype=tf.float64),
@@ -33,9 +34,7 @@ if __name__ == "__main__":
 
     def boundary(self, model, data):
         y_0_t = model(tf.stack([self.v['c']['0'], data], axis=1))
-        y_L_t = self.d(wrt={'x': 3},
-                       model=model,
-                       data=tf.stack([self.v['c']['L'], data], axis=1)) + 0.1 * tf.math.sin(2.0 * np.pi * 10.0 * data)
+        y_L_t = model(tf.stack([self.v['c']['L'], data], axis=1)) - 0.1 * tf.math.sin(2.0 * np.pi * 10.0 * data)
         dy_dx_0_t = self.d(wrt={'x': 1},
                            model=model,
                            data=tf.stack([self.v['c']['0'], data], axis=1))
