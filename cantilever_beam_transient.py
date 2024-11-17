cd7d3931584cf7a8dd982b504063ae1acc74c52a
Jesse Modesto <jesse.modesto.resume@protonmail.com>
2024-11-17 12:52:58 -0800

    try tanhx + sechx for activation func

--- cantilever_beam_transient.py
+++ cantilever_beam_transient.py
@@ -1,9 +1,14 @@
 from main import PINN
 import tensorflow as tf
 import numpy as np
+import pandas as pd
 import types
+from keras import backend as K
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.utils import get_custom_objects
 
 if __name__ == "__main__":
+    ansys = pd.read_csv('TransientDplZ.txt')
     solution = np.array(
         [0.0, 1.99328638928e-07, 7.82537085797e-07, 1.7293456267e-06, 3.01947488879e-06,
          4.63264723294e-06, 6.54858649796e-06, 8.74702072906e-06, 1.12076813821e-05, 1.39103076435e-05,
@@ -11,6 +16,11 @@ if __name__ == "__main__":
          3.40738806699e-05, 3.79038210667e-05, 4.18140771217e-05, 4.5784519898e-05, 4.97950641147e-05,
          5.38256463187e-05],
         dtype=np.float64)
+
+    def custom_activation(x):
+        return tf.math.tanh(x) + tf.math.cosh(x)**-1
+
+    get_custom_objects().update({'custom': custom_activation})
     pinn = PINN(  # constants declared here so that they are not constructed each time in functions
         constants={'rho': tf.constant(value=2770.0, shape=(1, 1), dtype=tf.float64),
                    'E': tf.constant(value=7.1 * 10 ** 10, shape=(1, 1), dtype=tf.float64),
@@ -23,11 +33,9 @@ if __name__ == "__main__":
                               't': np.array([1.00288032714], dtype=np.float64)},
               'boundary': {'t': np.array([1.00288032714], dtype=np.float64)},
               'test': {'x': np.linspace(start=0., stop=2.0, num=21, dtype=np.float64),
-                       't': np.array([1.00288032714], dtype=np.float64)},
-              'hybrid': {'x': np.linspace(start=0., stop=2.0, num=21, dtype=np.float64),
-                         't': np.array([1.00288032714], dtype=np.float64)}},
+                       't': np.array([1.00288032714], dtype=np.float64)}},
         layers=[10, 15, 15, 10, 1],  # neuron per layers
-        activation_function='tanh')
+        activation_function=custom_activation)
 
 
     def collocation(self, model, data):
@@ -57,10 +65,6 @@ if __name__ == "__main__":
         return y_0_t, dy_dx_0_t, d2y_dx2_L_t, d3y_dx3_L_t  # dy_dt_0_t, d2y_dt2_L_t
 
 
-    def hybrid(self, model, data):
-        return tf.reduce_sum(tf.square(model(data) - self.v['c']['solution']))
-
-
     def equation(self, model, data):
         return self.v['c']['solution'] - model(data)
 
@@ -74,20 +78,17 @@ if __name__ == "__main__":
     def loss_function_batch(self, model, train_data, index):
         loss = tf.reduce_mean(tf.square(self.collocation(model, train_data['collocation'][index])))
         loss += tf.reduce_sum([tf.square(bc) for bc in self.boundary(model, train_data['boundary'][0])])
-        loss += self.hybrid(model, train_data['hybrid'][0])
         return loss
 
 
     pinn.collocation = types.MethodType(collocation, pinn)
     pinn.boundary = types.MethodType(boundary, pinn)
-    pinn.hybrid = types.MethodType(hybrid, pinn)
     pinn.equation = types.MethodType(equation, pinn)
     pinn.loss_function = types.MethodType(loss_function, pinn)
     pinn.loss_function_batch = types.MethodType(loss_function_batch, pinn)
     pinn.train_network(epochs=10000,
                        batches={'collocation': 16,
-                                'boundary': 1,
-                                'hybrid': 21},
+                                'boundary': 1},
                        plot_x='x',
                        test_error=10 ** -7)
     input()
