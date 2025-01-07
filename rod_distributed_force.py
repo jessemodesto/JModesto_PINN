beda5157f80a8515ee90a7568e265f6271730e09
Jesse Modesto <jesse.modesto.resume@protonmail.com>
2025-01-06 18:01:10 -0800

    update rod case

--- rod_distributed_force.py
+++ rod_distributed_force.py
@@ -2,59 +2,46 @@ from main import PINN
 import tensorflow as tf
 import numpy as np
 import types
-import csv
 
 if __name__ == "__main__":
     pinn = PINN(
         constants={'c': tf.constant(1000.0, shape=(1, 1), dtype=tf.float64),
+                   # 'c': tf.constant(1.0, shape=(1, 1), dtype=tf.float64),
                    'A': tf.constant(value=np.pi * 10 ** -4., shape=(1, 1), dtype=tf.float64),
+                   # 'A': tf.constant(value=1.0, shape=(1, 1), dtype=tf.float64),
                    'E': tf.constant(value=7.1 * 10 ** 10, shape=(1, 1), dtype=tf.float64),
+                   # 'E': tf.constant(value=1.0, shape=(1, 1), dtype=tf.float64),
                    'L': tf.constant(1.0, shape=(1, 1), dtype=tf.float64),
-                   'zero': tf.constant(0.0, shape=(1, 1), dtype=tf.float64),
-                   'length': tf.constant(1.0, shape=(1, 1), dtype=tf.float64)},
-        data={'collocation': {'x': np.random.uniform(low=0., high=1.0, size=501)},
-              'boundary': {'x': np.array([0], dtype=np.float64)},
+                   '0': tf.constant(0.0, shape=(1, 1), dtype=tf.float64)},
+        data={'collocation': {'x': np.random.uniform(low=0., high=1.0, size=1000)},
               'test': {'x': np.linspace(start=0.0, stop=1.0, num=101, dtype=np.float64)}},
-        layers=[5, 10, 15, 10, 5, 1],  # neuron per layers
-        activation_function='tanh')  # activation function
-
+        layers=[15, 30, 60, 30, 15, 1],
+        activation_function='tanh',
+        guess=0)
 
     def collocation(self, model, data):
-        value = self.v['c']['A'] * self.v['c']['E'] * self.d(wrt={'x': 2}, model=model, data=data) + self.v['c']['c'] * data
-
+        value = self.d(wrt={'x': 2}, model=model, data=data) + \
+                self.v['c']['c'] * data / (self.v['c']['A'] * self.v['c']['E'])
         return value
 
-
-    def boundary(self, model, _):
-        fix = model(self.v['c']['zero'])
+    def boundary(self, model):
+        fix = model(self.v['c']['0'])
         end = self.d(wrt={'x': 1}, model=model, data=self.v['c']['L'])
         return fix, end
-
 
     def equation(self, data):
         return (self.v['c']['c'] / self.v['c']['A'] / self.v['c']['E'] / 6 *
                 (-data ** 3 + (3 * data * self.v['c']['L'] ** 2)))
 
-
-    def loss_function_batch(self, model, train_data, index):
-        loss = tf.reduce_mean(tf.square(self.collocation(model, train_data['collocation'][index])))
-        with open('collocation.csv', 'a', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows([[loss.numpy()]])
-        bcs = [tf.square(bc) for bc in self.boundary(model, train_data['boundary'][0])]
-        loss += tf.reduce_sum(bcs)
-        with open('bcs.csv', 'a', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows([[bc.numpy()[0][0] for bc in bcs]])
+    def loss_function(self, model, train_data):
+        loss = tf.reduce_mean(tf.square(self.collocation(model, train_data['collocation'])))
+        loss += tf.reduce_sum(tf.square(self.boundary(model)))
         return loss
-
 
     pinn.collocation = types.MethodType(collocation, pinn)
     pinn.boundary = types.MethodType(boundary, pinn)
     pinn.equation = types.MethodType(equation, pinn)
-    pinn.loss_function_batch = types.MethodType(loss_function_batch, pinn)
-    print(pinn.train_network(epochs=10000,
-                             batches={'collocation': 32,
-                                      'boundary': 1},
-                             test_error=10 ** -7,
-                             plot_x='x'))
+    pinn.loss_function = types.MethodType(loss_function, pinn)
+    pinn.train_network(epochs=10000,
+                       test_error=10 ** -2,
+                       plot_x='x')
