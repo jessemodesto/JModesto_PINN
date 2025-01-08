8a038e4e648e84132e64f8de60520714463cb78c
Jesse Modesto <jesse.modesto.resume@protonmail.com>
2025-01-07 19:15:47 -0800

    update simply supported cantilever beam

--- cantilever_simply_supported_beam_distributed_load.py
+++ cantilever_simply_supported_beam_distributed_load.py
@@ -5,57 +5,44 @@ import types
 
 if __name__ == "__main__":
     pinn = PINN(
-        constants={'I': tf.constant(1.0, shape=(1, 1), dtype=tf.float64),
-                   'E': tf.constant(1.0, shape=(1, 1), dtype=tf.float64),
+        constants={'I': tf.constant(value=np.pi * 0.01 ** 4 / 4., shape=(1, 1), dtype=tf.float64),
+                   # 'I': tf.constant(value=1.0, shape=(1, 1), dtype=tf.float64),
+                   'E': tf.constant(value=7.1 * 10 ** 10, shape=(1, 1), dtype=tf.float64),
+                   # 'E': tf.constant(value=1.0, shape=(1, 1), dtype=tf.float64),
                    'L': tf.constant(1.0, shape=(1, 1), dtype=tf.float64),
-                   'w': tf.constant(1.0, shape=(1, 1), dtype=tf.float64),
-                   'zero': tf.constant(0.0, shape=(1, 1), dtype=tf.float64),
-                   'length': tf.constant(1.0, shape=(1, 1), dtype=tf.float64)},
+                   'w': tf.constant(1000.0, shape=(1, 1), dtype=tf.float64),
+                   # 'w': tf.constant(1.0, shape=(1, 1), dtype=tf.float64),
+                   '0': tf.constant(0.0, shape=(1, 1), dtype=tf.float64)},
         data={'collocation': {'x': np.random.uniform(low=0., high=1.0, size=1000)},
-              'boundary': {'x': np.array([0], dtype=np.float64)},
-              'test': {'x': np.linspace(start=0.0, stop=1.0, num=11, dtype=np.float64)}},
-        layers=[15, 25, 15, 1],  # neuron per layers
-        activation_function='tanh')  # activation function
-
+              'test': {'x': np.linspace(start=0.0, stop=1.0, num=101, dtype=np.float64)}},
+        layers=[15, 30, 60, 30, 15, 1],
+        activation_function='tanh',
+        guess=0)
 
     def collocation(self, model, data):
-        value = (self.v['c']['w'] / self.v['c']['E'] / self.v['c']['I'] +
-                 self.d(wrt={'x': 4},
-                        model=model,
-                        data=data))
+        value = self.d(wrt={'x': 4}, model=model, data=data) + \
+                self.v['c']['w'] / self.v['c']['E'] / self.v['c']['I']
         return value
 
-
-    def boundary(self, model, _):
-        y_0 = model(self.v['c']['zero'])
+    def boundary(self, model):
+        y_0 = model(self.v['c']['0'])
         y_L = model(self.v['c']['L'])
-        dy_dx_0 = self.d(wrt={'x': 1},
-                         model=model,
-                         data=self.v['c']['zero'])
-        d2y_dx2_L = self.d(wrt={'x': 2},
-                           model=model,
-                           data=self.v['c']['L'])
+        dy_dx_0 = self.d(wrt={'x': 1}, model=model, data=self.v['c']['0'])
+        d2y_dx2_L = self.d(wrt={'x': 2}, model=model, data=self.v['c']['L'])
         return y_0, y_L, dy_dx_0, d2y_dx2_L
 
+    def equation(self, data):
+        return self.v['c']['w'] / self.v['c']['E'] / self.v['c']['I'] / 24.0 * (-data ** 4 + 5.0 / 2.0 * self.v['c']['L'] * data ** 3 - 3.0 / 2.0 * data ** 2)
 
-    def equation(self, model, data):
-        return (self.v['c']['w'] / self.v['c']['E'] / self.v['c']['I'] / 24.0 *
-                (-data ** 4 + 5.0 / 2.0 * self.v['c']['L'] * data ** 3 - 3.0 / 2.0 * data ** 2)) - model(
-            data)
-
-
-    def loss_function_batch(self, model, train_data, index):
-        loss = tf.reduce_mean(tf.square(self.collocation(model, train_data['collocation'][index])))
-        loss += tf.reduce_sum([tf.reduce_mean(tf.square(bc)) for bc in self.boundary(model, train_data['boundary'][0])])
+    def loss_function(self, model, train_data):
+        loss = tf.reduce_mean(tf.square(self.collocation(model, train_data['collocation'])))
+        loss += tf.reduce_sum(tf.square(self.boundary(model)))
         return loss
-
 
     pinn.collocation = types.MethodType(collocation, pinn)
     pinn.boundary = types.MethodType(boundary, pinn)
     pinn.equation = types.MethodType(equation, pinn)
-    pinn.loss_function_batch = types.MethodType(loss_function_batch, pinn)
-    pinn.train_network(epochs=10000,
-                       batches={'collocation': 16,
-                                'boundary': 1},
-                       plot_x='x',
-                       test_error=10 ** -7)
+    pinn.loss_function = types.MethodType(loss_function, pinn)
+    pinn.train_network(epochs=1000000,
+                       test_error=10 ** -2,
+                       plot_x='x')
